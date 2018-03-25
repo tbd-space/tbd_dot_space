9f17072a1c851edd1cae69f618e4918b27f588b7
Dave Cole <dave@wistia.com>
2018-03-25 15:14:21 -0400

    Create minimal sign up and log in flow with Auth0

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 The public site for tbd_.
 
-## Running in dev
+## Setting things up
 
 1. `git clone`
 2. `npm install`
-3. `npm run start`
 
-## Deploying
+## Running in dev
 
-1. `now`
+1. `npm run dev`
+
+## Deploying to staging
+
+1. `now --dotenv=.env.staging`
+2. Go to https://manage.auth0.com/#/clients, select the tbd "regular web application" client.
+3. In Settings, add "[the now deployment URL on your clipboard]/auth0-callback" to Allowed Callback URLs. This will make it possible to sign in on that staging URL. Hoping to find a better way to do this, but Auth0 doesn't allow wildcards in that field, the `now` deployments all get unique URLs (a new one for each [immutable deployment](https://zeit.co/docs/other/faq#how-do-i-update-my-deployment's-files-or-code)).
+
+
+## Deploying to production
+
+1. `now --dotenv=.env.production`
 2. `now alias [url that now copied to your clipboard] tbd.space`
--- server.js
+++ server.js
@@ -1,22 +1,72 @@
-// server.js
-// where your node app starts
+'use strict';
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// Configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL || `${process.env.NOW_URL}/auth0-callback`
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+// This can be used to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'shhhhhhhhh',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// hbs is the handlebars view engine for express.
+// https://github.com/pillarjs/hbs
+// When we call response.render(), it'll render and respond with an html page
+// generated from handlebars templates.
+const hbs = require('hbs');
+const hbsutils = require('hbs-utils')(hbs);
+app.set('view engine', 'hbs');
+hbsutils.registerPartials(__dirname + '/views/partials');
+hbsutils.registerWatchedPartials(__dirname + '/views/partials');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+
+const index = require("./routes/index");
+app.use("/", index);
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
